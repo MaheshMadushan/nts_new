514d303cbbbd2970ce5d7d02a4cc10b9d97ec4c7
Mahesh Madushan <madu140@gmail.com>
2020-12-14 05:53:38 +0530

    result table name not updating error fixed

--- Model/db_update_profileInfo.php
+++ Model/db_update_profileInfo.php
@@ -9,8 +9,6 @@ $NIC = $_POST['NIC'];
 $lname = $_POST['lname'];
 $UserSessionName = $_POST['UserSessionName'];
 
-
-
 $sql = "UPDATE user";
 $sql .= " SET ";
 $sql .= "first_name=?,";
@@ -25,11 +23,31 @@ if (!mysqli_stmt_prepare($stmt, $sql)) {
     echo "sql statement error";
     printf("fatal error.please contact Admin immideately");
     exit();
+    
 } else {
     mysqli_stmt_prepare($stmt, $sql);
     mysqli_stmt_bind_param($stmt, 'sssss', $name, $email, $NIC, $lname, $UserSessionName);
     mysqli_stmt_execute($stmt);
     printf("rows updated: %d\n", mysqli_stmt_affected_rows($stmt));
 }
+
+    $sql2 = "UPDATE result";
+    $sql2 .= " SET ";
+    $sql2 .= "first_name=?,";
+    $sql2 .= "last_name=?";
+    $sql2 .= " WHERE ";
+    $sql2 .= "index_no=?";
+
+    $stmt2 = mysqli_stmt_init($db);
+    if (!mysqli_stmt_prepare($stmt2, $sql2)) {
+        echo "sql statement error";
+        printf("fatal error.please contact Admin immideately");
+        exit();
+    } else {
+        mysqli_stmt_prepare($stmt2, $sql2);
+        mysqli_stmt_bind_param($stmt2, 'sss', $name, $lname, $UserSessionName);
+        mysqli_stmt_execute($stmt2);
+        printf("rows updated: %d\n", mysqli_stmt_affected_rows($stmt2));
+    }
 
 $db->close();
